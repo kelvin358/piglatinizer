6730f545a480e7003b87df0252daf8b53ce9c732
kelvin358 <kelvinli358@gmail.com>
2019-10-16 01:25:41 +0000

    insert comment

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
 $( "document" ).ready(function() {
 
-
+$('#button').click(function(){
+    //let str = $('.input').val();
+    //$(.output).text(piglatin(str));
+    $('.output').text('.input');
 
 });
+
+function piglatin(str){
+  var firstLetter = str[0];
+  var vowels = ['a', 'e', 'i', 'o', 'u'];
+
+  if (vowels.includes(firstLetter)){
+    return str + 'ay';
+}
+  else
+    str.slice(1);
+    return str + firstLetter + 'ay';
+};
+
+});
+
+//python3 -m http.server 3000
